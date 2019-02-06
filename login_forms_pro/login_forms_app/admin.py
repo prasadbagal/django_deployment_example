55db5941fdbbdbe0f26d0599210fb45ea1bc6a6d
prasadbagal <bagalprasad@gmail.com>
2019-02-06 21:48:35 +0530

    first commit

---
+++ login_forms_pro/login_forms_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from login_forms_app.models import UserProfileInfo
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ login_forms_pro/login_forms_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LoginFormsAppConfig(AppConfig):
+    name = 'login_forms_app'
